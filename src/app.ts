20323ad4368b0fa9fa7a4d3e5ecc56befb7b1d32
tranmanhthang96nd <tranmanhthang@cryptopie-labo.com>
2022-07-13 17:47:10 +0700

    adding a droppable area

--- src/app.ts
+++ src/app.ts
@@ -189,7 +189,8 @@ class ProjectItem
 
   @Autobind
   dragStartHandler(event: DragEvent) {
-    console.log('dragStart', event);
+    event.dataTransfer!.setData('text/plain', this.project.id)
+    event.dataTransfer!.effectAllowed = 'move'
   }
 
   @Autobind
@@ -227,10 +228,15 @@ class ProjectList
 
   @Autobind
   dragOverHandler(event: DragEvent) {
-    const listEl = this.element.querySelector('ul')!;
-    listEl.classList.add('droppable');
+    if (event.dataTransfer && event.dataTransfer.types[0] === 'text/plain') {
+      event.preventDefault();
+      const listEl = this.element.querySelector('ul')!;
+      listEl.classList.add('droppable');
+    }
   }
-  dropHandler(event: DragEvent) {}
+  dropHandler(event: DragEvent) {
+    console.log(event.dataTransfer!.getData('text/plain'))
+  }
 
   @Autobind
   dragLeaveHandler(event: DragEvent) {
