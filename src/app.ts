b5c808285bf1ef59daefa1194696a9df1d30dfb1
tranmanhthang96nd <tranmanhthang@cryptopie-labo.com>
2022-07-13 14:06:24 +0700

    filtering projects with enums

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,7 @@ class Project {
 }
 
 // Project State Management
-type Listener = (items: Project[]) => void
+type Listener = (items: Project[]) => void;
 
 class ProjectState {
   private listeners: Listener[] = [];
@@ -133,7 +133,13 @@ class ProjectList {
     this.element.id = `${this.type}-projects`;
 
     projectState.addListener((projects: Project[]) => {
-      this.assignedProjects = projects;
+      const relevantProjects = projects.filter((prj) => {
+        if (this.type === 'active') {
+          return prj.status === ProjectStatus.Active;
+        }
+        return prj.status === ProjectStatus.Finished;
+      });
+      this.assignedProjects = relevantProjects;
       this.renderProjects();
     });
 
@@ -142,11 +148,12 @@ class ProjectList {
   }
 
   private renderProjects() {
-    const listEl = document.getElementById(`${this.type}-projects-list`);
+    const listEl = document.getElementById(`${this.type}-projects-list`) as HTMLUListElement;
+    listEl.innerHTML = '';
     for (const prjItem of this.assignedProjects) {
       const listItem = document.createElement('li');
       listItem.textContent = prjItem.title;
-      listEl?.appendChild(listItem);
+      listEl.appendChild(listItem);
     }
   }
 
