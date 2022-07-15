d7ec978d866d5618396c8bbc32cfe8986c345b73
tranmanhthang96nd <tranmanhthang@cryptopie-labo.com>
2022-07-15 10:39:33 +0700

    problem with namespace imports

--- src/components/project-list.ts
+++ src/components/project-list.ts
@@ -1,3 +1,8 @@
+/// <reference path="base-component.ts" />
+/// <reference path="../models/project.ts" />
+/// <reference path="../models/drag-drop.ts" />
+/// <reference path="../decorators/autobind.ts" />
+/// <reference path="../state/project.ts" />
 namespace App {
   // ProjectList
   export class ProjectList
